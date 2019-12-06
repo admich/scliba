4657c3a0042337a3c767b536e79dca0c9cc97fec
admich <andrea.demichele@gmail.com>
2019-12-06 10:36:17 +0100

    some skeleton

--- scliba-mode.el
+++ scliba-mode.el
@@ -8,3 +8,21 @@
   "Keymap for scliba-mode.")
 
 (define-key scliba-mode-map "\C-c\C-c"  'scliba-compila-guarda-file)
+
+
+(define-skeleton scliba-skeleton-pq-format
+  "Box per chiedere il Nome degli alunni nelle verifiche"
+  nil
+  "(pq-format " _ ")")
+
+(define-skeleton scliba-skeleton-imath
+  "Box per chiedere il Nome degli alunni nelle verifiche"
+  nil
+  "(imath " _ ")")
+
+(define-abbrev-table 'scliba-mode-abbrev-table
+  '(("pqf" "" scliba-skeleton-pq-format)
+    ("imath" "" scliba-skeleton-imath)))
+
+
+
